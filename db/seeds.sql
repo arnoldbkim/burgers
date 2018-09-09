e96cea92d590c121f0b4d8888327e7b5f07324c4
Arnold <arnoldbkim@gmail.com>
2018-09-08 23:55:12 -0700

    hw

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('hamburger', false);
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('cheeseburger', false);
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('double hamburger', false);
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('double cheeseburger', false);
